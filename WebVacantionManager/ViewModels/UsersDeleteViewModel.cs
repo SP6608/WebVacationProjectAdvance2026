847fa3daac3ab4f3b2270a9b6969fa3d65fd7cc0
SP6608 <spapanov@gmail.com>
2026-03-14 19:34:30 +0200

    End

---
+++ WebVacantionManager/ViewModels/UsersDeleteViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace WebVacantionManager.ViewModels
+{
+    public class UsersDeleteViewModel
+    {
+        public string Id { get; set; } = null!;
+
+        public string UserName { get; set; } = null!;
+
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        public string? RoleName { get; set; }
+
+        public string? TeamName { get; set; }
+    }
+}
